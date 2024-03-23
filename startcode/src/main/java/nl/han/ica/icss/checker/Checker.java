73b564f97b80f53379cb66a51fa1b23fa040bbd2
MrPotatoezzz <JW.Snijders@student.han.nl>
2024-03-23 14:59:36 +0100

    CheckerOfVariableBestaat

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -2,7 +2,6 @@ package nl.han.ica.icss.checker;
 
 import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
-import nl.han.ica.datastructures.Node;
 import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.literals.*;
 import nl.han.ica.icss.ast.types.ExpressionType;
@@ -12,27 +11,34 @@ import java.util.HashMap;
 
 public class Checker {
     private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
+    private IHANLinkedList<VariableReference> ReferencesInCurrentScope = new HANLinkedList<>();
     HashMap<String, ExpressionType> hashMap = new HashMap<>();
 
     public void check(AST ast) {
         variableTypes = new HANLinkedList<>();
-        checkTree(ast.root);
+        checkASTNode(ast.root, null);
         variableTypes.insert(variableTypes.getSize() - 1, hashMap);
+
     }
 
-    public void checkTree(ASTNode node) {
+    public void checkASTNode(ASTNode node, ASTNode parent) {
         if (node == null) {
+            //ReferencesInCurrentScope = null;
             return;
         }
 
         if (node.getClass() == Stylerule.class) {
-            //Oude hashmap toevoegen aan lijst
             if (variableTypes.getSize() == 0) {
                 variableTypes.addFirst(hashMap);
             } else {
                 variableTypes.insert(variableTypes.getSize() - 1, hashMap);
             }
             hashMap = new HashMap<>();
+
+            if(ReferencesInCurrentScope.getSize() != 0) {
+                checkIfReferencedVariableExists(ReferencesInCurrentScope.getFirst());
+                ReferencesInCurrentScope = new HANLinkedList<>();
+            }
         }
 
         ExpressionType expressionType = ExpressionType.UNDEFINED;
@@ -40,14 +46,50 @@ public class Checker {
             expressionType = setExpressionType(((Declaration) node).expression);
         } else if (node instanceof VariableAssignment){
             expressionType = setExpressionType(((VariableAssignment) node).expression);
+        }else if (node instanceof VariableReference){
+            if(ReferencesInCurrentScope.getSize() == 0){
+                ReferencesInCurrentScope.addFirst((VariableReference) node);
+            }else {
+                ReferencesInCurrentScope.insert(ReferencesInCurrentScope.getSize() - 1, (VariableReference) node);
+            }
         }
 
         if (node.getClass() == VariableAssignment.class) {
-            hashMap.put(node.getNodeLabel(), expressionType);
+            hashMap.put(((VariableAssignment) node).name.name, expressionType);
         }
 
         for (ASTNode childNode : node.getChildren()) {
-            checkTree(childNode);
+            checkASTNode(childNode, node);
+        }
+    }
+
+
+    private void checkIfReferencedVariableExists(ASTNode node){
+        if(ReferencesInCurrentScope.getSize() == 0){
+            return;
+        }
+        ExpressionType expressionType = ExpressionType.UNDEFINED;
+        VariableReference reference = (VariableReference) node;
+        boolean foundNote = false;
+        boolean correctType = false;
+        for (int i = 0; i < variableTypes.getSize(); i++) {
+            if (variableTypes.get(i).containsKey(reference.name)) {
+                expressionType = setExpressionType(((VariableReference) node));
+                if (variableTypes.get(i).get(reference.name) == expressionType) {
+                    correctType = true;
+                }
+                foundNote = true;
+            }
+        }
+
+        if (!foundNote) {
+            node.setError("Variable: " + reference.name + " undefined");
+        } else if (!correctType && expressionType != ExpressionType.UNDEFINED) {
+            node.setError("Can't change " + reference.name + " to type " + expressionType);
+        }
+        ReferencesInCurrentScope.delete(0);
+        if(ReferencesInCurrentScope.getSize() != 0) {
+            checkIfReferencedVariableExists(ReferencesInCurrentScope.getFirst());
         }
     }
 
